5d86a42777901347a73a2fc943c5f306a34ef518
Gabriela Medrano <gmedranob@miumg.edu.gt>
2026-03-05 20:17:11 -0600

    Estructura inicial del programa con menús y métodos vacios

--- src/principal/Main.java
+++ src/principal/Main.java
@@ -1,10 +1,237 @@
 package principal;
 
+import java.util.Scanner;
+
 public class Main {
 
+	static Scanner sc = new Scanner(System.in);
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("===== REPASO PRIMER PARCIAL =====");
+        System.out.println("       Gabriela Medrano\n");
 
+         menuPrincipal();
 	}
 
-}
+	public static void menuPrincipal() {
+		int opcion;
+		
+		do {
+			System.out.println("\n===== MENU PRINCIPAL =====");	
+			System.out.println("1. Parte 1 – Ciclos y Operaciones Numericas");
+			System.out.println("2. Parte 2 – Condiciones y Divisibilidad");
+		    System.out.println("3. Parte 3 – Uso de Switch");
+		    System.out.println("4. Parte 4 – Analisis Numerico");
+		    System.out.println("5. Parte 5 – Patrones y Transformaciones");
+		    System.out.println("6. Salir");
+
+		    System.out.print("Seleccione una opcion: ");
+		    opcion = sc.nextInt();
+		
+		    switch(opcion) {
+
+            case 1:menuParte1(); break;
+            case 2:menuParte2(); break;
+            case 3:menuParte3(); break;
+            case 4:menuParte4(); break;
+            case 5:menuParte5();break;
+            case 6:System.out.println("Saliendo del programa...");  break;
+            default:
+                System.out.println("Opcion invalida");
+        }
+
+    } while(opcion != 6);
+
+	}
+	
+	// SUBMENU PARTE 1
+	public static void menuParte1() {
+
+	    int opcion;
+
+	    do {
+
+	        System.out.println("\n===== PARTE 1 – CICLOS =====");
+	        System.out.println("1. Suma de divisibles entre 4");
+	        System.out.println("2. Factorial");
+	        System.out.println("3. Conteo positivos, negativos y ceros");
+	        System.out.println("4. Promedio de pares");
+	        System.out.println("5. Suma de digitos");
+	        System.out.println("6. Regresar");
+
+	        System.out.print("Seleccione una opcion: ");
+	        opcion = sc.nextInt();
+
+	        switch(opcion) {
+		 
+	        case 1:ejercicio1(); break;
+            case 2:ejercicio2(); break;
+            case 3:ejercicio3(); break;
+            case 4:ejercicio4(); break;
+            case 5:ejercicio5(); break;
+	        }
+
+	    } while(opcion != 6);
+
+	}  
+			
+	public static void ejercicio1() {}
+	public static void ejercicio2() {}
+	public static void ejercicio3() {}
+	public static void ejercicio4() {}
+	public static void ejercicio5() {}
+	
+	
+	// SUBMENU PARTE 2
+	public static void menuParte2() {
+
+	    int opcion;
+
+	    do {
+
+	        System.out.println("\n===== PARTE 2 =====");
+	        System.out.println("1. Multiplo de 7 o 11");
+	        System.out.println("2. Divisibles entre 3 pero no entre 2");
+	        System.out.println("3. Clasificacion de hora");
+	        System.out.println("4. Regresar");
+
+	        System.out.print("Seleccione una opcion: ");
+	        opcion = sc.nextInt();
+
+	        switch(opcion) {
+
+	            case 1: ejercicio6(); break;
+	            case 2: ejercicio7(); break;
+	            case 3: ejercicio8(); break;
+
+	        }
+
+	    } while(opcion != 4);
+
+	}
+	
+	public static void ejercicio6() {}
+	public static void ejercicio7() {}
+	public static void ejercicio8() {}
+	
+	//SUBMENU PARTE 3
+	public static void menuParte3() {
+
+	    int opcion;
+
+	    do {
+
+	        System.out.println("\n===== PARTE 3 – SWITCH =====");
+	        System.out.println("1. Numero romano");
+	        System.out.println("2. Calculadora");
+	        System.out.println("3. Regresar");
+
+	        System.out.print("Seleccione una opcion: ");
+	        opcion = sc.nextInt();
+
+	        switch(opcion) {
+
+	            case 1: ejercicio9(); break;
+	            case 2: ejercicio10(); break;
+
+	        }
+
+	    } while(opcion != 3);
+
+	}
+	
+	public static void ejercicio9() {}
+	public static void ejercicio10() {}
+	
+	
+	//SUBMENU PARTE 4
+	public static void menuParte4() {
+
+	    int opcion;
+
+	    do {
+
+	        System.out.println("\n===== PARTE 4 =====");
+	        System.out.println("1. Mayor, menor y diferencia");
+	        System.out.println("2. Tabla de division");
+	        System.out.println("3. Digitos pares");
+	        System.out.println("4. Numero triangular");
+	        System.out.println("5. Promedio negativos");
+	        System.out.println("6. Regresar");
+
+	        System.out.print("Seleccione una opcion: ");
+	        opcion = sc.nextInt();
+
+	        switch(opcion) {
+
+	            case 1: ejercicio11(); break;
+	            case 2: ejercicio12(); break;
+	            case 3: ejercicio13(); break;
+	            case 4: ejercicio14(); break;
+	            case 5: ejercicio15(); break;
+
+	        }
+
+	    } while(opcion != 6);
+
+	}
+	
+	public static void ejercicio11() {}
+	public static void ejercicio12() {}
+	public static void ejercicio13() {}
+	public static void ejercicio14() {}
+	public static void ejercicio15() {}	
+	
+	//SUBMENU PARTE 5 
+	public static void menuParte5() {
+
+	    int opcion;
+
+	    do {
+
+	        System.out.println("\n===== PARTE 5 =====");
+	        System.out.println("1. Multiplos de 6");
+	        System.out.println("2. Potencia de 2");
+	        System.out.println("3. Numero invertido");
+	        System.out.println("4. Suma 1 a n par o impar");
+	        System.out.println("5. Celsius a Fahrenheit");
+	        System.out.println("6. Regresar");
+
+	        System.out.print("Seleccione una opcion: ");
+	        opcion = sc.nextInt();
+
+	        switch(opcion) {
+
+	            case 1: ejercicio16(); break;
+	            case 2: ejercicio17(); break;
+	            case 3: ejercicio18(); break;
+	            case 4: ejercicio19(); break;
+	            case 5: ejercicio20(); break;
+
+	        }
+
+	    } while(opcion != 6);
+
+	}
+	 
+	public static void ejercicio16() {}
+	public static void ejercicio17() {}
+	public static void ejercicio18() {}
+	public static void ejercicio19() {}
+	public static void ejercicio20() {}
+
+	}
+	
+	
+	
+	
+
+	     
+	
+
+	
+	
+	
+	
+
